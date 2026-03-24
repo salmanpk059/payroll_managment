29c62a8de08eb119163a2f7ee6beefc3697c0ecb
[salmanpk059] <[salmanpk059@gmail.com]>
2026-03-24 14:41:51 +0500

    change vits

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: "payroll_managment",
+    base: "/payroll_managment/",
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
